04c399aec369e556493a6e66534f17b78c5420db
MalteNord <malte.nordstrand@gmail.com>
2024-11-26 20:56:10 +0100

    added eslint remove in config

--- cms24-delta-nextjs/next.config.mjs
+++ cms24-delta-nextjs/next.config.mjs
@@ -1,15 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    async redirects() {
-      return [
-        {
-          source: '/',
-          destination: '/sv',
-          permanent: false, // Change to true if you want a permanent redirect
-        },
-      ];
-    },
-  };
-  
-  export default nextConfig;
-  
+  eslint: {
+    ignoreDuringBuilds: true, // Disables ESLint during the build process
+  },
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/sv',
+        permanent: false, // Change to true if you want a permanent redirect
+      },
+    ];
+  },
+};
+
+export default nextConfig;
